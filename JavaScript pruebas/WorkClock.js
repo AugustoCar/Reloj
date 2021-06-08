8444b69cc793cfc9341444200baec069172ee298
Augusto <augustocardenas96@gmail.com>
2021-06-08 15:14:15 -0300

    Funciona reloj
    ahora tengo q hacer alarmas

--- JavaScript pruebas/WorkClock.js
+++ JavaScript pruebas/WorkClock.js
@@ -1,9 +1,24 @@
-(function(){
+var hr = document.getElementById('hr');
+var min = document.getElementById('min');
+var sec = document.getElementById('sec');
 
+
+function initClock(){
+
+    var date = new Date();
+    var hour = date.getHours() % 12;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+
+    var hourDeg = hour * 30;
+    var minuteDeg = minute * 6;
+    var secondDeg = second * 6;
     
-   var mn = 1000 * 60 ;
-   var hr = mn * 60;
-   
-    
-    
-});
+    hr.style.transform = 'rotate(' + hourDeg + 'deg)';
+    mn.style.transform = 'rotate(' + minuteDeg + 'deg)'; 
+    sc.style.transform = 'rotate(' + secondDeg + 'deg)'; 
+
+    setTimeout(initClock, 1000);
+};
+
+initClock();
