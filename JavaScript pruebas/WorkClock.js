e61887b59a94455ad2f07b0a4f8093158951f11a
Augusto <augustocardenas96@gmail.com>
2021-06-09 20:48:08 -0300

    set de hora para la alarma
    falta hacer q suene y fijarse porq el clear no funciona

--- JavaScript pruebas/WorkClock.js
+++ JavaScript pruebas/WorkClock.js
@@ -1,3 +1,8 @@
+//sound of the alarm
+var sound = new Audio("https://www.youtube.com/watch?v=8xz9s_-sO1I&ab_channel=CriticalRoleCriticalRoleVerificada");
+sound.loop = true;
+
+//setting the time of the clock with the computer 
 var hr = document.getElementById('hr');
 var min = document.getElementById('min');
 var sec = document.getElementById('sec');
@@ -22,3 +27,58 @@ function initClock(){
 };
 
 initClock();
+
+//functions to get hour, min, secs, am or pm, add zero, set alarm time and sound, clear alarm
+
+function addZero(time) {
+
+    return (time < 10) ? "0" + time : time;
+
+}
+
+function hoursMenu(){
+ var select = document.getElementById('alarmHr');
+
+
+ var hrs = 12;
+
+ for(i = 1; i <= hrs; i++){
+
+    select.options[select.options.length] = new Option(i < i ? "0" + 1: i, i);
+    }
+
+}hoursMenu();
+
+function minsMenu(){
+ var select = document.getElementById('alarmMin');
+
+
+ var min = 59;
+
+ for(i = 0; i <= min; i++){
+
+    select.options[select.options.length] = new Option(i < i ? "0" + 1: i, i);
+    }
+
+}minsMenu();
+
+function secsMenu(){
+ var select = document.getElementById('alarmSec');
+
+
+ var sec = 59;
+
+ for(i = 0; i <= sec; i++){
+
+    select.options[select.options.length] = new Option(i < i ? "0" + 1: i, i);
+    }
+
+}secsMenu();
+
+function alarmClear() {
+
+	document.getElementById('alarmHr').disabled = false;
+	document.getElementById('alarmMin').disabled = false;
+	document.getElementById('alarmSec').disabled = false;
+	sound.pause();
+}
